01c0819198a1ae4d77c7059c06558023db3692e1
efra8ab <46880165+efra8ab@users.noreply.github.com>
2024-03-09 02:29:40 -0600

    March 9

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Reflection;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string stgPercent = Console.ReadLine();
+        int intPercent = int.Parse(stgPercent);
+        string letter = "";
+
+        int remainder = intPercent % 10;
+        string sign =  "";
+
+        if (remainder >= 7 && intPercent < 97 && intPercent >= 60)
+        {
+            sign = "+";
+        }
+        else if (remainder < 3 && intPercent >= 60)
+        {
+            sign = "-";
+        }
+
+        if (intPercent >= 90 && intPercent < 100)
+        {
+            letter = "A";
+        }
+        else if (intPercent >= 80 && intPercent < 90)
+        {
+            letter = "B";
+        }
+        else if (intPercent >= 70 && intPercent < 80)
+        {
+            letter = "C";
+        }
+        else if (intPercent >= 60 && intPercent < 70)
+        {
+            letter = "D";
+        }
+        else if (intPercent < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("The value entered is incorrect ");
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        if (intPercent >= 70)
+        {
+            Console.WriteLine("You passed the class!!");
+        }
+        else
+        {
+            Console.WriteLine("You have failed the class, try again next time");
+        }
+    
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World!!!!");
+        int x;
+        x = 3;
+        Console.WriteLine(x);
+
+        Console.WriteLine("What is your favorite color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your favorite color is {color}");
+        int y = 1;
+
+        if (x > y)
+        {
+            Console.WriteLine("graeater");
+        }
+        
     }
 }
