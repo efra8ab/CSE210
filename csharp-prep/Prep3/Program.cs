d3dc64b28f546b52f8f3bf1950505912110ae7a4
efra8ab <46880165+efra8ab@users.noreply.github.com>
2024-03-11 00:40:30 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        string playAgain = "yes";
+        do
+        {   
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int counter = 1;
+
+            Console.WriteLine($"What is the Magic number? {magicNumber}");
+            Console.Write("Make a guess: ");
+            string guess = Console.ReadLine();
+            int intGuess = int.Parse(guess);    
+
+            while (magicNumber != intGuess)
+            {
+                counter += 1;
+
+                if (magicNumber < intGuess)
+                {Console.WriteLine("Lower");}
+                else if (magicNumber > intGuess)
+                {Console.WriteLine("Higher");}
+
+                Console.Write("Make a guess: ");
+                guess = Console.ReadLine();
+                intGuess = int.Parse(guess);
+            }
+
+            Console.WriteLine("\nYou guessed it!");
+            Console.WriteLine($"only took you {counter} tries");
+
+            Console.WriteLine("\nDo you want to play again? ");
+            playAgain = Console.ReadLine();
+
+        }while (playAgain == "yes");
+
+
     }
 }
