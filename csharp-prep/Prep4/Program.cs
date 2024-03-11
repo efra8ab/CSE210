8eed04d50ae2bb80f26d2e2f7a3545bd5f1d33a2
efra8ab <46880165+efra8ab@users.noreply.github.com>
2024-03-11 14:37:57 -0600

    final prep

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        List<int> numbers = new List<int>();
+        
+        int newNumber = 1;
+
+        Console.WriteLine("Enter a list of number, type 0 when finished.");
+
+        while (newNumber != 0)
+        {
+            Console.Write("Next number ");
+            string userInput = Console.ReadLine();
+            newNumber = int.Parse(userInput);
+            if (newNumber != 0)
+            {
+                numbers.Add(newNumber);
+            }
+        }
+
+        int sumOfList = 0; 
+        float avgOfList = 0;
+        int minOfList = int.MaxValue;
+        int maxOfList = int.MinValue;
+
+
+        foreach(int number in numbers)
+        {   
+            //sum of all the elements on the list
+            sumOfList += number;
+            Console.WriteLine(number);
+
+            //finding the largest number on the list
+            if (number > maxOfList)
+            {
+                maxOfList = number;
+            }
+
+            //finding the smallest number on the list
+            else if (number < minOfList && number > 0)
+            {
+                minOfList = number;
+            }
+        }
+
+        //obtaining the average of the numbers inside the list
+        avgOfList = (float)sumOfList / numbers.Count;
+        
+        Console.WriteLine($"\nSum of the list: {sumOfList}");
+        Console.WriteLine($"Avg of the list: {avgOfList}");
+        Console.WriteLine($"Largest number of the list: {maxOfList}");
+        Console.WriteLine($"Smallest positive number of the list: {minOfList}");
+
+        //sorting the list
+        numbers.Sort();
+
+        //printing the sorted  list
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -3,7 +3,42 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    {   
+        DisplayWelcome();
+        string username = PromptUserName();
+        int favoriteNum = PromptUserNumber();
+        int sqrdNum = SquareNumber(favoriteNum);
+        DisplayResult(username, sqrdNum);
     }
+
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("What is your username?: ");
+            string username = Console.ReadLine();
+            return username;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("What is your favorite number?: ");
+            string userInput = Console.ReadLine();
+            int favoriteNum = int.Parse(userInput); 
+            return favoriteNum;
+        }
+
+        static int SquareNumber(int favoriteNum)
+        {
+            int sqrdNum = favoriteNum * favoriteNum;
+            return sqrdNum;
+        }
+
+        static void DisplayResult(string username, int sqrdNum)
+        {
+            Console.WriteLine($"{username}, the square of your number is {sqrdNum}");
+        }
 }
