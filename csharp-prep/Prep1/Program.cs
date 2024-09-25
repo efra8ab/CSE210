13c4d953b527efdfcd14788c85ce05b2144deb35
efra8ab <46880165+efra8ab@users.noreply.github.com>
2024-09-25 16:04:29 -0600

    finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your First Name? ");
+        string FirstName = Console.ReadLine();
+        Console.Write("What is your Last Name? ");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {LastName}, {FirstName} {LastName}.");
     }
 }
