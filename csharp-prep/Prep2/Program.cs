d348addd7520d51d16cf8f0f61fd0717f1630037
efra8ab <46880165+efra8ab@users.noreply.github.com>
2024-09-25 22:30:03 -0600

    completed excercise 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? %");
+        string GradePercentage = Console.ReadLine();
+
+        int grade = int.Parse(GradePercentage);
+        string letter;
+        string modifier = "";
+
+        if (grade >= 90)
+        {letter = "A";}
+        else if (grade >= 80)
+        {letter = "B";}
+        else if (grade >= 70)
+        {letter = "C";}
+        else if (grade >= 60)
+        {letter = "D";}
+        else
+        {letter = "F";}
+
+        if (letter != "F" && grade < 97)
+        {
+            int LastDigit = grade % 10;
+
+            if (LastDigit >= 7)
+            {modifier = "+";}
+
+            else if (LastDigit <= 3)
+            {modifier = "-";}
+        }
+
+        Console.WriteLine($"\nYou've got {letter}{modifier}");
+
+        if (grade < 70)
+        {Console.WriteLine("Unfortunately, you have failed the class. Next luck next time ");}
+        else
+        {Console.WriteLine("Congratulations, You have passed this class!");}
     }
 }
