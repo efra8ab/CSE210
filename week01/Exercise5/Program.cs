e3b43f23bc0a9c2e57d0722a43be6ddcecfadb96
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-01-12 16:31:17 -0600

    Exercise 5 complete

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,48 @@ using System;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Clear();
+        Console.Write("Write down your username: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Clear();
+        Console.Write("Write down your favorite number: ");
+        string strUserNumber = Console.ReadLine();
+        int userNumber = int.Parse(strUserNumber);
+        return userNumber;
+    }
+
+    static int SquareNumber(int userNumber)
+    {
+        int squareNumber = userNumber * userNumber;
+        return squareNumber;
+    }
+
+    static void DisplayResult(string userName, int SquareNumber)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {SquareNumber}");
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.Clear();
+        DisplayWelcome();
+        string username = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int SquareNum = SquareNumber(userNumber);
+        DisplayResult(username, SquareNum);
+
     }
 }
