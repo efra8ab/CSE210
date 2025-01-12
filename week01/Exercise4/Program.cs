422cf031d7e1ee1952fa1cfcddc97dcd54f768a6
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-01-12 03:05:47 -0600

    Exercise 4 - Core requirements

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.Clear();
+
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (true)
+        {
+            Console.WriteLine("Enter number: ");
+            string strNumber = Console.ReadLine();
+            int number = int.Parse(strNumber);
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            
+            else if (number == 0)
+            {
+                break;
+            }
+
+        }
+
+        //SUM of values
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        //Average
+        double avg = (double)sum / numbers.Count;
+
+        //Smallest
+        int min = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number < min) {min = number;}
+        }
+
+        //Biggest
+        int max = numbers[0];
+        foreach(int number in numbers)
+        {
+            if (number > max) {max = number;}
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The largest number is: {max}");
+        Console.WriteLine($"The smallest number is: {min}");
+        
     }
 }
