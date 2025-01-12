596316d52ae6bdace61d35308ff97f3523d642bd
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-01-12 01:34:39 -0600

    excercise 1 completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Clear();
+
+        Console.WriteLine("What is your first name?");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        Console.Clear();
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
     }
 }
