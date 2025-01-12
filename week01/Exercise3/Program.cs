25d59f87c16c44591c2a5a65e4d48e7422b16765
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-01-12 02:41:06 -0600

    Exercise 3 - Stretch challenges

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,12 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.Clear();
+        
+
+    while (true)
+    {   
         Random random= new Random();
         int magicNumber = random.Next(0, 100);
 
+        int counter = 0;
+
+        Console.WriteLine("Welcome to the Magic Number game, lets play!");
+
         while (true)
         {
-
+            counter += 1;
             Console.Write("Take a guess: ");
             string strGuess = Console.ReadLine();
             int guess = int.Parse(strGuess);
@@ -20,7 +28,32 @@ class Program
             else if (guess > magicNumber)
             {Console.WriteLine("lower!");}
             else
-            {Console.WriteLine("You got it!"); break;}
+            {
+                Console.Clear();
+                Console.WriteLine("You got it!");
+                Console.WriteLine($"number of guesses: {counter}");
+                Console.WriteLine("Press enter to continue ...");
+                Console.ReadLine(); 
+                break;
+            }
         }
+        Console.Clear();
+        Console.WriteLine("Do you wish to play again? (y / n)");
+        string playAgain = Console.ReadLine().ToLower();
+
+        if (playAgain == "n")
+        {
+            Console.Clear();
+            Console.WriteLine("Thanks for playing!");
+            break;
+        }
+        else if (playAgain != "y")
+        {
+            Console.Clear();
+            Console.WriteLine("Invalid input. Closing game!");
+            break;
+        }
+        Console.Clear();
+    }
     }
 }
