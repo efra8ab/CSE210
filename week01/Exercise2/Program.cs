6e86ec32eb6980fd3f777829cd9e735ccf30d06f
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-01-12 01:54:43 -0600

    Exercise 2 - Core Requirements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("Write down your grade percentage: ");
+        string strGrade = Console.ReadLine();
+        int grade = int.Parse(strGrade);
+
+        string letterGrade = "";
+
+        if (grade >= 90) // A
+        {
+            letterGrade = "A";
+        } 
+
+        else if (grade >= 80) // B
+        {
+            letterGrade = "B";
+        }
+
+        else if (grade >= 70) // C
+        {
+            letterGrade = "C";
+        }
+
+        else if (grade >= 60) // D
+        {
+            letterGrade = "D";
+        }
+
+        else if (grade < 60) // F
+        {
+            letterGrade = "F";
+        }
+
+        else
+        {   
+            Console.Clear();
+            Console.WriteLine("Please enter a valid percentage!");
+        }
+
+        if (grade >= 70)
+        {
+            Console.Clear();
+            Console.WriteLine($"Letter grade: {letterGrade}\nCongratulations!, you have passed the course");
+        }
+        else 
+        {
+            Console.Clear();
+            Console.WriteLine($"Letter grade: {letterGrade}\nUnfortunately, you have not passed the course");
+        }
+
     }
 }
