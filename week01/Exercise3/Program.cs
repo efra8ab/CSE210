1c8ea999c65db308af062a12cfdeb3e6654acfcd
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-01-12 02:25:59 -0600

    Exercise 3 - Core requirements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Clear();
+        Random random= new Random();
+        int magicNumber = random.Next(0, 100);
+
+        while (true)
+        {
+
+            Console.Write("Take a guess: ");
+            string strGuess = Console.ReadLine();
+            int guess = int.Parse(strGuess);
+
+            if (guess < magicNumber)
+            {Console.WriteLine("higher!");}
+            else if (guess > magicNumber)
+            {Console.WriteLine("lower!");}
+            else
+            {Console.WriteLine("You got it!"); break;}
+        }
     }
 }
