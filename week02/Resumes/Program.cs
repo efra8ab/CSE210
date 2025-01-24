f35b9de87882e94531df99006a179d774bb32b1a
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-01-24 01:12:14 -0600

    Resume code complete

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Console.Clear();
+
+        var Job1 = new Job();
+        var Job2 = new Job();
+
+        Job2._company = "Microsoft";
+        Job2._jobTitle = "Supervisor";
+        Job2._startYear= 2021;
+        Job2._endYear= 2024;
+
+        Job1._company = "Apple";
+        Job1._jobTitle = "Software Development";
+        Job1._startYear= 2017;
+        Job1._endYear= 2021;
+        
+        var Resume = new Resume();
+        Resume._name = "Efrain O";
+        Resume._jobs.Add(Job1);
+        Resume._jobs.Add(Job2);
+
+        Resume.Display();
     }
 }
