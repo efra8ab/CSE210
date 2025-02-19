c3a9b1a8e0552f7654d887e5d252be620801b088
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-02-19 00:14:57 -0600

    Assignments completed

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Console.Clear();
+        
+        MathAssignment mathAssignment= new MathAssignment("Efrain O", "Fractions", "12.b", "13 - 15");
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine("\n");
+
+        WrittingAssignment writtingAssignment = new WrittingAssignment("Eric N", "WW2", "External Causes");
+        Console.WriteLine(writtingAssignment.GetWrittingInformation());
+
     }
 }
---
+++ week05/Homework/WrittingAssignment.cs
@@ -0,0 +1,14 @@
+public class WrittingAssignment : Assignment
+{
+    private string _title;
+
+    public WrittingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWrittingInformation()
+    {
+        return $"{base.GetSummary()}\n{_title} by {GetStudentName()}";
+    }
+}
