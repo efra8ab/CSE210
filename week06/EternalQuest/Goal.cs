f0be9cda65a7a12411e4dcf82c6531a602d16432
efra8ab <46880165+efra8ab@users.noreply.github.com>
2025-02-20 16:02:34 -0600

    Simple Goal

--- week06/EternalQuest/Goal.cs
+++ week06/EternalQuest/Goal.cs
@@ -11,13 +11,13 @@ public abstract class Goal
         _points = points;
     }
 
-    public abstract int RecordEvent();
+    public abstract void RecordEvent();
 
     public abstract bool IsComplete();
 
     public abstract string GetDetailsString();
 
     public abstract string GetStringRepresentation();
-        
+
 
 }
---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,31 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) : base (name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public override void RecordEvent()
+    {
+        _isComplete = true;
+        Console.WriteLine($"Goal '{_shortName}' completed! You earned {_points} points.");
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetDetailsString()
+    {
+        return $"[{(IsComplete() ? "X" : " ")}] {_shortName} - {_description} ({_points} points)";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal|{_shortName}|{_description}|{_points}|{_isComplete}";
+    }
+
+}
